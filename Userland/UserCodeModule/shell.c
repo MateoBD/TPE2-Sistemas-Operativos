aa17cb5302ed27c89cf4e9f4d9958221c2f3337d
naprado <naprado@itba.edu.ar>
2025-06-09 17:35:42 -0300

    commands

--- Userland/UserCodeModule/shell.c
+++ Userland/UserCodeModule/shell.c
@@ -37,6 +37,8 @@ command_t commands[] = {
     {"help", help, "Show available commands"},
     {"exit", exit_shell, "Exit the shell"},
     {"cat", cat, "Concatenate and print files to stdout"},
+    {"wc", wc, "Count lines, words, and characters in input (use 'wc --help' for options)"},
+    {"filter", filter, "Filter out vowels from input"},
     {"test", test_command, "Run tests (use 'test help' for options)"},
     {NULL, NULL, NULL} // Sentinel
 };
