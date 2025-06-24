20e050812e9bd54b2e4218bbd5bf1114ef14b2c4
Mateo Buela <120684119+MateoBD@users.noreply.github.com>
2025-06-23 23:41:54 -0300

    fix: mem cmd is now shell built-in

--- Userland/UserCodeModule/programs/mem.c
+++ Userland/UserCodeModule/programs/mem.c
@@ -4,16 +4,11 @@
 #include <gnauni.h>
 #include <process-handler.h>
 
-static void print_memory_state(int argc, char **argv){
+void mem(int argc, char **argv, uint16_t * fds){
     HeapState state;
     get_heap_state(&state);
     printf("Memory manager: %s\n", state.mm_type);
     printf("Total memory: %d\n", state.total_memory);
     printf("Used memory:  %d\n", state.used_memory);
     printf("Free memory:  %d\n", state.free_memory);
-    exit(0);
-}
-
-void mem(int argc, char **argv, uint16_t * fds){
-    process_handler("mem", print_memory_state, argc, argv, fds);
 }
